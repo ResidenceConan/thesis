b80a90215a446293c73a3aa9d46ec325003d361e
Robin Suter <suter.robin@gmail.com>
2018-06-05 12:55:52 +0200

    update code listing

--- projectdoc/listing/nearest_neighbor.sql
+++ projectdoc/listing/nearest_neighbor.sql
@@ -1,21 +1,29 @@
-  FOR nearest_vertex IN
+FOR nearest_vertex IN
+  WITH nearest_bboxes AS (
+      SELECT id, the_geom
+      FROM routing_segmented_vertices_pgr v
+      -- get nearest points in approximation
+      ORDER BY the_geom <#> ST_SetSRID(ST_MakePoint(stop_lon, stop_lat), 4326)
+      LIMIT 100
+  )
   SELECT id
-  FROM routing_segmented_vertices_pgr v
-  -- Order points by distance to transport stop
-  ORDER BY v.the_geom <#> ST_SetSRID(ST_MakePoint(stop_lon, stop_lat), 4326)
-  LIMIT 100
-  LOOP
-    SELECT max(agg_cost)
-    INTO max_distance
-    -- Get all points reachable in 200 Meters
-    FROM pgr_drivingDistance(
-        'SELECT id, source, target, cost FROM edge_preselection',
-        nearest_vertex,
-        200,
-        FALSE);
+  FROM nearest_bboxes
+  -- order by nearest points (precise)
+  ORDER BY ST_Distance(
+    the_geom,
+    ST_SetSRID(ST_MakePoint(stop_lon, stop_lat), 4326))
+LOOP
+  SELECT max(agg_cost)
+  INTO max_distance
+  -- Get all points reachable in 200 Meters
+  FROM pgr_drivingDistance(
+      'SELECT id, source, target, cost FROM edge_preselection',
+      nearest_vertex,
+      200,
+      FALSE);
 
-    IF max_distance >= 150
-    THEN
-      RETURN nearest_vertex;
-    END IF;
-  END LOOP;
+  IF max_distance >= 150
+  THEN
+    RETURN nearest_vertex;
+  END IF;
+END LOOP;
